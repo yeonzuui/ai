1e22d49a66b8236883771c6f5da37d96f422c4e6
yeonzuui <0123sksk@gmail.com>
2024-09-27 17:59:09 +0900

    VIEW_TOP-N & INDEX & TRANSACTION

---
+++ source/02_DBMS/mysql실습.sql
@@ -0,0 +1,6 @@
+-- DCL(계정생성, 권한부여, 권한박탈, 계정삭제)
+-- DDL(제약조건FK, SEQUENCE 없음)
+-- DML(outer join, and = &&,  or = ||(연결연산자는 concat 사용)일부단일행함수, rownum 없어 top-N 구문이 다름)
+
+-- java에서 쓸 데이터 넣어 보고 연습해 보기
+show databases;
