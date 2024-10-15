5d0ab0b95698da7a0e1c3e45a5d8b9ccb81ccca9
yeonzuui <0123sksk@gmail.com>
2024-10-15 18:00:52 +0900

    js 기본문법, 조건문, 반복문

---
+++ source/04_JavaScript/ch02_기본문법/5_false들.js
@@ -0,0 +1,17 @@
+// false로 해석되는 값: 0, ''(공백X), undefined, NaN, null
+var i;
+// if(i){
+//     console.log('true다');
+// } else{
+//     console.log('false다');
+// }
+console.log(Boolean(i));
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(NaN));
+console.log(Boolean(null));
+console.log(); // 개행
+console.log(Boolean(-999));
+
+console.log("빈스트링===false의 결과: " + (""===false));
+console.log("빈스트링==false의 결과: " + (""==false));
---
+++ source/04_JavaScript/ch02_기본문법/6_숫자로  표현할 수 없는 수.js
@@ -0,0 +1,8 @@
+// , 
+i = parseInt("");
+console.log('i타입은 ', typeof(i), ', 값은', i);
+a = 10/0; // 10/0.00000000000000000000...
+console.log('a타입은 ', typeof(a), ', 값은', a);
+console.log('i가 NaN인지 여부: ', isNaN(i));
+console.log('a가 NaN인지 여부: ', isNaN(a));
+console.log('a가 유한한 수인지 여부: ', isFinite(a));
