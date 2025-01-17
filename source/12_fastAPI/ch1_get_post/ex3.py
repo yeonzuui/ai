9614653d486431643de4ad3f1d3ffe96a0c70800
yeonzuui <0123sksk@gmail.com>
2025-01-17 12:24:23 +0900

    flask fileupload

--- source/12_fastAPI/ch1_get_post/ex3.py
+++ source/12_fastAPI/ch1_get_post/ex3.py
@@ -11,6 +11,13 @@ from pydantic import BaseModel
 # POST 방식으로 들어온 form 데이터 받을 때 필요
 from fastapi import Form
 
+# 유효성 검사
+# BaseModel 로부터 상속받은 User
+class User(BaseModel):
+    name : str
+    id : str
+    pw : int
+
 # FastAPI 객체 생성
 app = FastAPI()
 
@@ -25,9 +32,40 @@ app.mount("/static",
 @app.get('/')
 
 @app.get('/html1')
-async def html1(request:Request):
-    # ex3-1.html에 rendering
-    # 매개변수로 request 전달
+# ex3-1.html에 rendering
+# 매개변수로 request 전달
+async def html1(request:Request): # Type Hint
     return templates.TemplateResponse("ex3-1.html",
                                       {'request':request})
+
+@app.get('/html2')
+# ex3-2.html에 rendering
+# 매개변수로 request 전달
+async def html2(request:Request): # Type Hint
+    return templates.TemplateResponse("ex3-2.html",
+                                      {'request':request,
+                                       'name':'홍길동'})
+
+# @app.get('/html3')
+# # json 으로 return
+# # 매개변수 request 불필요
+# async def html3(name:str, id:str, pw:int):
+#     return {'name':name, 'id':id, 'pw':pw}
+
+# POST 방식으로 받기
+# pip install python-multipart # 필수
+@app.post('/html3')
+# = Form(): Form 태그로부터 받음
+async def html3(user:User = Form()):
+    # return user.dict()
+    return {'name':user.name,
+            'id':user.id,
+            'pw':user.pw}
+# Postman 에서 post 방법으로 검증 시, [Body] - [form-data]로 바꾸고 parameter 입력
+
+# async def html3(name:str = Form(),
+#                 id:str = Form(),
+#                 pw:int = Form()):
+#     return {'name': name, 'id': id, 'pw': pw}
+
 
---
+++ source/12_fastAPI/ch2_fileupload/main.py
@@ -0,0 +1,98 @@
+# pip install fastapi
+# pip install uvicorn
+# pip install jinja2           # template 으로 rendering
+# pip install python-multipart # POST 로 전달 시 필수
+from os import access
+
+from Tools.scripts.win_add2path import modify
+# template 사용
+from fastapi import FastAPI, Request
+# 파일 첨부
+from fastapi import File, UploadFile
+# static mounting
+from fastapi.staticfiles import StaticFiles
+# template 지정
+from fastapi.templating import Jinja2Templates
+# redirect: 사용자가 처음 요청한 URL이 아닌, 다른 URL로 가라고 지시
+from starlette.responses import RedirectResponse
+# 예외 발생
+from starlette.exceptions import HTTPException
+# 파일 다운로드
+from fastapi.responses import FileResponse
+
+import os
+import datetime
+
+# FastAPI 객체 생성
+app = FastAPI()
+# static mounting
+app.mount('/static', StaticFiles(directory = 'static'), name = 'static')
+# template 지정
+templates = Jinja2Templates(directory = "templates")
+
+UPLOAD_FOLDER = './upload/'
+# 디렉토리가 없을 때만 생성
+os.makedirs(UPLOAD_FOLDER,
+            exist_ok = True)
+
+def stamp2real(stamp):
+    return datetime.datetime.fromtimestamp(stamp)
+
+def info(filename):
+    create   = stamp2real(os.path.getctime(filename))
+    modify   = stamp2real(os.path.getmtime(filename))
+    access   = stamp2real(os.path.getatime(filename))
+    tempsize = os.path.getsize(filename)
+    size = ''
+    if tempsize < 1024 * 1024:
+        size = '{:.2f}KB'.format(tempsize / 1024)
+    else:
+        size = '{:.2f}MB'.format(tempsize / (1024 * 1024))
+    return create, modify, access, size
+
+@app.get('/')
+@app.post('/')
+async def index(request:Request):
+    # 업로드 폴더에 있는 파일 list
+    filelist = os.listdir(UPLOAD_FOLDER)
+    infos = []
+    for filename in filelist:
+        create, modify, access, size = info(UPLOAD_FOLDER + filename)
+        infos.append({
+            'name':filename,
+            'create':create,
+            'modify':modify,
+            'size':size
+        })
+    return templates.TemplateResponse("home.html",
+                                      {'request':request,
+                                       'infos':infos})
+
+# 파일 업로드
+@app.post('/upload')
+async def upload(request:Request,
+                 file:UploadFile = File()): # 파라미터 이름은 input 태그의 name="file"과 같아야 함
+    # 파일 첨부했을 경우
+    if file.filename:
+        # file_path = UPLOAD_FOLDER + file.filename
+        file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        with open(file_path, 'wb') as buffer: # stream
+            # await: 비동기적으로 read() 함수 수행하고 저장
+            buffer.write(await file.read())
+        create, modify, access, size = info(file_path)
+        fileinfo = {'ctime':create,
+                    'size':size}
+        return templates.TemplateResponse('check.html',
+                                          {'request': request,
+                                           'fileinfo':fileinfo})
+    # 파일 첨부 안 했을 경우
+    else:
+        # 405 Error: '/' 경로로 redirect 시 POST 방식을 찾을 수 없다고 뜰 수 있음, POST 방식 추가해주기
+        return RedirectResponse(url = '/',
+                                status_code = 307)
+
+# 삭제는 반드시 DELETE 방식으로 전달
+@app.get('/del/{filename}')
+async def delete(filename:str):
+    os.remove(UPLOAD_FOLDER + filename)
+    return RedirectResponse('/')
